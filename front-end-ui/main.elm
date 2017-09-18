dd07e3c86d7dfb202c98c9beefe5caddcc8f4f04
hugorifbjerg <hugo@toogoodtogo.com>
2017-09-18 16:04:42 +0200

    Added modal, not working

--- front-end-ui/main.elm
+++ front-end-ui/main.elm
@@ -5,39 +5,73 @@ import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Button as Button
 import Bootstrap.Table as Table
-
-
-type Msg
-    = CreateUser
-    | CancelCreateUser
+import Bootstrap.Modal as Modal
+import Bootstrap.Form.Input as Input
 
 
 type alias Model =
-    Int
+    { modalState : Modal.State }
 
 
-model : Model
-model =
-    0
+init : ( Model, Cmd Msg )
+init =
+    ( { modalSate = Modal.hiddenState }, Cmd.none )
 
 
-update : Msg -> Model -> Model
+type Msg
+    = ModalMsg Modal.State
+
+
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        CreateUser ->
-            model + 1
-
-        CancelCreateUser ->
-            model - 1
+        ModelMsg state ->
+            ( { model | modalState = state }, Cmd.none )
 
 
-view : Model -> Html Msg
+view : Model -> Html msg
 view model =
     Grid.container []
         [ CDN.stylesheet
         , Grid.row []
             [ Grid.col []
-                [ Button.button [ Button.primary, Button.onClick CreateUser ] [ text ("Opret bruger" ++ (toString model)) ] ]
+                [ Button.button [ Button.primary, Button.onClick ModalMsg Modal.visibleState ] [ text "Opret bruger" ] ]
+            , Modal.config ModalMsg
+                Modal.small
+                Modal.h3
+                []
+                [ text "Opret bruger" ]
+                Modal.body
+                []
+                [ Grid.containerFluid []
+                    [ Grid.row []
+                        [ Grid.col [ Col.sm4 ] [ text "Brugername" ]
+                        , Grid.col [ Col.sm8 ] [ Input.text [ Input.attrs [ placeholder "brugernavn" ] ] ]
+                        ]
+                    , Grid.row []
+                        [ Grid.col [ Col.sm4 ] [ text "Telefon nummer" ]
+                        , Grid.col [ Col.sm8 ] [ Input.text [ Input.attrs [ placeholder "000" ] ] ]
+                        ]
+                    , Grid.row []
+                        [ Grid.col [ Col.sm4 ] [ text "For- og efternavn" ]
+                        , Grid.col [ Col.sm8 ] [ Input.text [ Input.attrs [ placeholder "Hans Peter Jensen" ] ] ]
+                        ]
+                    , Grid.row []
+                        [ Grid.col [ Col.sm4 ] [ text "Adresse" ]
+                        , Grid.col [ Col.sm8 ] [ Input.text [ Input.attrs [ placeholder "et eller andet vej 555" ] ] ]
+                        ]
+                    ]
+                ]
+                Modal.footer
+                []
+                [ Button.button
+                    [ Button.outlinePrimary
+                    , Button.attr [ onClick ModalMsg Modal.hiddenState ]
+                    ]
+                    [ text "Close" ]
+                ]
+                Modal.view
+                model.modalState
             ]
         , Grid.row []
             [ Grid.col []
